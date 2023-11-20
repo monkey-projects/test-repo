583404d03fb8b4c55fb66735184b44d512297769
Wout Neirynck <wout@monkey-projects.be>
2023-11-20 16:18:56 +0100

    Added readme

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,11 +1,17 @@
 (ns podman-basic
-  (:require [monkey.ci.build.core :as c]))
+  (:require [monkey.ci.build
+             [core :as c]
+             [shell :as s]]))
+
+(def inspect
+  (s/bash "ls -al && df -h"))
 
 (def simple-container
   {:container/image "docker.io/alpine:latest"
    :script ["echo Hello > output.txt"]})
 
 (c/defpipeline build
-  [simple-container])
+  [inspect
+   simple-container])
 
 [build]
---
+++ README.md
@@ -0,0 +1,3 @@
+# Test repo
+
+This is just a test repo that we're using for _MonkeyCI_.  Nothing to see here...
